e4e538d59416bf22230e24fc4092b67540a52575
TheMode <themode@outlook.fr>
2021-05-28 20:46:20 +0200

    Slighly improve varint reading

--- src/main/java/fr/themode/proxy/utils/ProtocolUtils.java
+++ src/main/java/fr/themode/proxy/utils/ProtocolUtils.java
@@ -4,20 +4,36 @@ import java.nio.ByteBuffer;
 
 public class ProtocolUtils {
 
-    public static int readVarInt(ByteBuffer byteBuffer, int maxRead) {
-        int i = 0;
-        for (int j = 0; j < maxRead; j++) {
-            final int k = byteBuffer.get();
-            i |= (k & 0x7F) << j * 7;
-            if ((k & 0x80) != 128) {
-                return i;
+    public static int readVarInt(ByteBuffer src) {
+        // Code from https://github.com/bazelbuild/bazel/blob/master/src/main/java/com/google/devtools/build/lib/util/VarInt.java
+        int tmp;
+        if ((tmp = src.get()) >= 0) {
+            return tmp;
+        }
+        int result = tmp & 0x7f;
+        if ((tmp = src.get()) >= 0) {
+            result |= tmp << 7;
+        } else {
+            result |= (tmp & 0x7f) << 7;
+            if ((tmp = src.get()) >= 0) {
+                result |= tmp << 14;
+            } else {
+                result |= (tmp & 0x7f) << 14;
+                if ((tmp = src.get()) >= 0) {
+                    result |= tmp << 21;
+                } else {
+                    result |= (tmp & 0x7f) << 21;
+                    result |= (tmp = src.get()) << 28;
+                    while (tmp < 0) {
+                        // We get into this loop only in the case of overflow.
+                        // By doing this, we can call getVarInt() instead of
+                        // getVarLong() when we only need an int.
+                        tmp = src.get();
+                    }
+                }
             }
         }
-        throw new RuntimeException("VarInt is too big");
-    }
-
-    public static int readVarInt(ByteBuffer byteBuffer) {
-        return readVarInt(byteBuffer, 3);
+        return result;
     }
 
     public static void writeVarInt(ByteBuffer buffer, int value) {
