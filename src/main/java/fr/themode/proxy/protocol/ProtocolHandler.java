64902aaa9cf55f746d7ce9ab224b974cf8f17d1c
TheMode <themode@outlook.fr>
2021-07-02 01:22:45 +0200

    Inline protocol handlers

--- src/main/java/fr/themode/proxy/protocol/ProtocolHandler.java
+++ src/main/java/fr/themode/proxy/protocol/ProtocolHandler.java
@@ -1,7 +1,8 @@
 package fr.themode.proxy.protocol;
 
+import fr.themode.proxy.State;
 import fr.themode.proxy.connection.ConnectionContext;
-import fr.themode.proxy.utils.ConnectionStateUtils;
+import fr.themode.proxy.utils.ProtocolUtils;
 
 import java.nio.ByteBuffer;
 
@@ -9,8 +10,41 @@ import java.nio.ByteBuffer;
  * Handles basic packets (e.g. game state) to ensure proper parsing.
  */
 public interface ProtocolHandler {
-    ProtocolHandler CLIENT = ConnectionStateUtils::handleClientState;
-    ProtocolHandler SERVER = ConnectionStateUtils::handleServerState;
+    ProtocolHandler CLIENT = (context, packetId, payload) -> {
+        final var state = context.getState();
+        if (state == State.HANDSHAKE && packetId == 0) {
+            // Change to Status/Login state
+            final int protocol = ProtocolUtils.readVarInt(payload);
+            final String address = ProtocolUtils.readString(payload, 255);
+            final short port = payload.getShort();
+            final int stateId = ProtocolUtils.readVarInt(payload);
+            final State nextState = switch (stateId) {
+                case 1 -> State.STATUS;
+                case 2 -> State.LOGIN;
+                default -> throw new IllegalStateException("Unexpected value: " + stateId);
+            };
+            context.setState(nextState);
+            context.getTargetContext().setState(nextState);
+        } else if (state == State.LOGIN && packetId == 0) {
+            // Change to Play state
+            context.setState(State.PLAY);
+        }
+    };
+    ProtocolHandler SERVER = (context, packetId, payload) -> {
+        final var state = context.getState();
+        if (state == State.LOGIN) {
+            // Change to Play State
+            if (packetId == 2) {
+                context.setState(State.PLAY);
+            } else if (packetId == 3) {
+                // Compression
+                final int threshold = ProtocolUtils.readVarInt(payload);
+                context.setCompression(threshold);
+                context.getTargetContext().setCompression(threshold);
+            }
+            context.setState(State.PLAY);
+        }
+    };
 
     void process(ConnectionContext context, int packetId, ByteBuffer payload);
 }
